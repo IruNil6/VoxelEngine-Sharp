53a3a1e7df6682ce25a4819cf3285f8d9c15ab60
Iru <Yura.sorokin4@yandex.ru>
2024-08-24 01:54:18 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server
+{
+    //TODO: Create server code for multiplayer
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World From Server!");
+        }
+    }
+}
